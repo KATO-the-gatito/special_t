6def959676ad21c94965509b1214541ec3b24e61
KATO-the-gatito <Admin@DESKTOP-A1GNFOA>
2026-04-09 20:01:31 +0300

    logic changed in do_action()

--- special_t.cpp
+++ special_t.cpp
@@ -35,26 +35,38 @@ special_t do_action(special_t first, special_t second, char action) {
     Min_Max min_max = first.size < second.size ? Min_Max<special_t>{first, second} : Min_Max<special_t>{second, first};
     special_t result(min_max.max.size + 1, false), spec1(min_max.max.size + 1, false), spec2(min_max.max.size + 1, false);
     spec1.setspec(first); spec2.setspec(second);
-    if (action == '-'){
+
+    switch (action)
+    {
+    case '-':
+    {
         for (int i = 0; i < spec2.size; i++) {
             spec2.bytes_array[i] = ~spec2.bytes_array[i];
         }
-        // std::cout << "spec2: " << spec2.bin() << "\n";
         special_t tmp_spec(min_max.max.size, false);
         tmp_spec.setval(1);
         spec2.setspec(do_action(spec2, tmp_spec, '+'));
         spec2.resize(min_max.max.size + 1);
-        // std::cout << "spec2 aft: " << spec2.bin() << "\n";
     }
-    unsigned short tmp = 0;
-    byte rem = 0;
+        // fall through <----------!
+    case '+':
+    {
+        unsigned short tmp = 0;
+        byte rem = 0;
 
-    for (int i = 0; i < min_max.min.size + 1; i++) {
-        tmp = (short)(spec1.bytes_array[i]) + (short)(spec2.bytes_array[i]) + (short)(rem);
-        result.bytes_array[i] = (byte)tmp;
-        rem = (tmp >> 8);
+        for (int i = 0; i < min_max.min.size + 1; i++) {
+            tmp = (short)(spec1.bytes_array[i]) + (short)(spec2.bytes_array[i]) + (short)(rem);
+            result.bytes_array[i] = (byte)tmp;
+            rem = (tmp >> 8);
+        }  
     }
-    return result;
+        break;
+    default:
+        perror("wrong action!");
+        break;
+    }
+
+    return result; 
 }
 
 special_t operator+ (special_t first, special_t second) { 
